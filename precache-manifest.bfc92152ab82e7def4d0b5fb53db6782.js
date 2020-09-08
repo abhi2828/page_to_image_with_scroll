75b7339d65e693a98c91c8dbea8f915c73495113
abhay2828 <abhi280896@gmail.com>
2020-09-08 15:05:03 +0530

    Updates

---
+++ precache-manifest.bfc92152ab82e7def4d0b5fb53db6782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5853ee3306215a72e19b7c72aab82dc6",
+    "url": "/page_to_image_with_scroll/index.html"
+  },
+  {
+    "revision": "f074441b35ab066d798e",
+    "url": "/page_to_image_with_scroll/static/css/main.ca6f190a.chunk.css"
+  },
+  {
+    "revision": "8676b91faf26ff407329",
+    "url": "/page_to_image_with_scroll/static/js/2.a60c4799.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/page_to_image_with_scroll/static/js/2.a60c4799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f074441b35ab066d798e",
+    "url": "/page_to_image_with_scroll/static/js/main.f5cd6c9a.chunk.js"
+  },
+  {
+    "revision": "ce6d21117d386d21a0c7",
+    "url": "/page_to_image_with_scroll/static/js/runtime-main.8889ec95.js"
+  },
+  {
+    "revision": "832a4d3ff41654d5d835bad73c2784d2",
+    "url": "/page_to_image_with_scroll/static/media/bg-4.832a4d3f.jpg"
+  }
+]);
