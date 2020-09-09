e91876861e13d659111fce61279be8e02f04397f
abhay2828 <abhi280896@gmail.com>
2020-09-09 16:23:44 +0530

    design for approvial 2

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -228,17 +228,17 @@ const Banner = () => {
                             <div className="col-md-2 SvgRow">
                                 <Technology className="svg" img={Demodata[7].img}/> 
                             </div>
-                            <div className="col-md-2 SvgRow">
+                            <div className="col-md-2 SvgRow" style={{marginLeft: '30px'}}>
                                 <Technology className="svg" img={Demodata[8].img}/>
                             </div>
-                            <div className="col-md-2 SvgRow">
+                            <div className="col-md-2 SvgRow" style={{marginLeft: '30px'}}>
                                 <Technology className="svg" img={Demodata[9].img}/>
                             </div>
-                            <div className="col-md-2 SvgRow">
+                            <div className="col-md-2 SvgRow" style={{marginLeft: '30px'}}>
                                 <Technology className="svg" img={Demodata[10].img}/>
                             </div>
-                            <div className="col-md-2 SvgRow">
-                                <Technology className="svg" img={Demodata[11].img}/>
+                            <div className="col-md-2 SvgRow" style={{marginLeft: '70px'}}>
+                                <Technology className="svg" img={Demodata[11].img} />
                             </div>
                             
                         </div>
